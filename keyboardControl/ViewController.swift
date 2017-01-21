bbd39392a1130b87ac1681a9cf2099e6e617446b
West Kraemer <WestKraemer@gmail.com>
2017-01-21 13:40:31 -0500

    update keyboard settings so that keyboard pops up when text field is selected by user.

--- keyboardControl/ViewController.swift
+++ keyboardControl/ViewController.swift
@@ -20,6 +20,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    
 }
 
